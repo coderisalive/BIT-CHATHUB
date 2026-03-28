8a115df606b8c34eb34db7607f7098a469f0f94e
bit15029 <bitpatna15029@gmail.com>
2026-03-28 21:54:30 +0530

    ffff

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ const io = new Server(server, {
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "http://localhost:5175",
       "https://bit-chathub.vercel.app"
     ],
     methods: ["GET", "POST"]
